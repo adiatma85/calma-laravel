085971de49e2ab87ebd7d4ad27ce26ab51c53cad
Ramdani Kurnia <adiatma85@gmail.com>
2021-10-11 22:16:39 +0700

    refix routes/web/php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +10,27 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+// Controllers
+use App\Http\Controllers\Admin\HomeController as AdminHomeController;
+use App\Http\Controllers\Admin\PermissionsController as AdminPermissionsControlelr;
+use App\Http\Controllers\Admin\RolesController as AdminRolesController;
+use App\Http\Controllers\Admin\UsersController as AdminUsersController;
+use App\Http\Controllers\Admin\MusicItemController as AdminMusicItemController;
+use App\Http\Controllers\Admin\PlaylistController as AdminPlaylistController;
+use App\Http\Controllers\Admin\MusicTopicController as AdminMusicTopicController;
+use App\Http\Controllers\Admin\CurhatanController as AdminCurhatanController;
+use App\Http\Controllers\Admin\CommentController as AdminCommentController;
+use App\Http\Controllers\Admin\JournalController as AdminJournalController;
+use App\Http\Controllers\Admin\MoodTrackerController as AdminMoodTrackerController;
+use App\Http\Controllers\Admin\MoodTrackerReasonController as AdminMoodTrackerReasonController;
+use App\Http\Controllers\Admin\JourneyController as AdminJourneyController;
+
+// Auth
+use App\Http\Controllers\Auth\ChangePasswordController as AuthChangePasswordController;
 
 Route::redirect('/', '/login');
 Route::get('/home', function () {
@@ -25,74 +43,83 @@ Route::get('/home', function () {
 
 Auth::routes(['register' => false]);
 
-Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
-    Route::get('/', 'HomeController@index')->name('home');
-    // Permissions
-    Route::delete('permissions/destroy', 'PermissionsController@massDestroy')->name('permissions.massDestroy');
-    Route::resource('permissions', 'PermissionsController');
+Route::prefix('admin')
+    ->as('admin.')
+    ->middleware(['auth'])
+    ->group(function () {
+        // Home Page
+        Route::get('/', [AdminHomeController::class, 'index'])->name('home');
 
-    // Roles
-    Route::delete('roles/destroy', 'RolesController@massDestroy')->name('roles.massDestroy');
-    Route::resource('roles', 'RolesController');
+        // Permissions
+        Route::delete('permissions/destroy', [AdminPermissionsControlelr::class, 'massDestroy'])->name('permissions.massDestroy');
+        Route::resource('permissions', AdminPermissionsControlelr::class);
 
-    // Users
-    Route::delete('users/destroy', 'UsersController@massDestroy')->name('users.massDestroy');
-    Route::resource('users', 'UsersController');
+        // Roles
+        Route::delete('roles/destroy', [AdminRolesController::class, 'massDestroy'])->name('roles.massDestroy');
+        Route::resource('roles', AdminRolesController::class);
 
-    // Music Item
-    Route::delete('music-items/destroy', 'MusicItemController@massDestroy')->name('music-items.massDestroy');
-    Route::post('music-items/media', 'MusicItemController@storeMedia')->name('music-items.storeMedia');
-    Route::post('music-items/ckmedia', 'MusicItemController@storeCKEditorImages')->name('music-items.storeCKEditorImages');
-    Route::resource('music-items', 'MusicItemController');
+        // Users
+        Route::delete('users/destroy', [AdminUsersController::class, 'massDestroy'])->name('users.massDestroy');
+        Route::resource('users', AdminUsersController::class);
 
-    // Playlist
-    Route::delete('playlists/destroy', 'PlaylistController@massDestroy')->name('playlists.massDestroy');
-    Route::post('playlists/media', 'PlaylistController@storeMedia')->name('playlists.storeMedia');
-    Route::post('playlists/ckmedia', 'PlaylistController@storeCKEditorImages')->name('playlists.storeCKEditorImages');
-    Route::resource('playlists', 'PlaylistController');
+        // Music Item
+        Route::delete('music-items/destroy', [AdminMusicItemController::class, 'massDestroy'])->name('music-items.massDestroy');
+        Route::post('music-items/media', [AdminMusicItemController::class, 'storeMedia'])->name('music-items.storeMedia');
+        Route::post('music-items/ckmedia', [AdminMusicItemController::class, 'storeCKEditorImages'])->name('music-items.storeCKEditorImages');
+        Route::resource('music-items', AdminMusicItemController::class);
 
-    // Music Topic
-    Route::delete('music-topics/destroy', 'MusicTopicController@massDestroy')->name('music-topics.massDestroy');
-    Route::post('music-topics/media', 'MusicTopicController@storeMedia')->name('music-topics.storeMedia');
-    Route::post('music-topics/ckmedia', 'MusicTopicController@storeCKEditorImages')->name('music-topics.storeCKEditorImages');
-    Route::resource('music-topics', 'MusicTopicController');
+        // Playlist
+        Route::delete('playlists/destroy', [AdminPlaylistController::class, "massDestroy"])->name('playlists.massDestroy');
+        Route::post('playlists/media', [AdminPlaylistController::class, "storeMedia"])->name('playlists.storeMedia');
+        Route::post('playlists/ckmedia', [AdminPlaylistController::class, "storeCKEditorImages"])->name('playlists.storeCKEditorImages');
+        Route::resource('playlists', AdminPlaylistController::class);
 
-    // Curhatan
-    Route::delete('curhatans/destroy', 'CurhatanController@massDestroy')->name('curhatans.massDestroy');
-    Route::post('curhatans/media', 'CurhatanController@storeMedia')->name('curhatans.storeMedia');
-    Route::post('curhatans/ckmedia', 'CurhatanController@storeCKEditorImages')->name('curhatans.storeCKEditorImages');
-    Route::resource('curhatans', 'CurhatanController');
+        // Music Topic
+        Route::delete('music-topics/destroy', [AdminMusicTopicController::class, 'massDestroy'])->name('music-topics.massDestroy');
+        Route::post('music-topics/media', [AdminMusicTopicController::class, 'storeMEdia'])->name('music-topics.storeMedia');
+        Route::post('music-topics/ckmedia', [AdminMusicTopicController::class, 'storeCKEditorImages'])->name('music-topics.storeCKEditorImages');
+        Route::resource('music-topics', AdminMusicTopicController::class);
 
-    // Comment
-    Route::delete('comments/destroy', 'CommentController@massDestroy')->name('comments.massDestroy');
-    Route::post('comments/media', 'CommentController@storeMedia')->name('comments.storeMedia');
-    Route::post('comments/ckmedia', 'CommentController@storeCKEditorImages')->name('comments.storeCKEditorImages');
-    Route::resource('comments', 'CommentController');
+        // Curhatan
+        Route::delete('curhatans/destroy', [AdminCurhatanController::class, 'massDestroy'])->name('curhatans.massDestroy');
+        Route::post('curhatans/media', [AdminCurhatanController::class, 'storeMedia'])->name('curhatans.storeMedia');
+        Route::post('curhatans/ckmedia', [AdminCurhatanController::class, 'storeCKEditorImages'])->name('curhatans.storeCKEditorImages');
+        Route::resource('curhatans', AdminCurhatanController::class);
 
-    // Journal
-    Route::delete('journals/destroy', 'JournalController@massDestroy')->name('journals.massDestroy');
-    Route::post('journals/media', 'JournalController@storeMedia')->name('journals.storeMedia');
-    Route::post('journals/ckmedia', 'JournalController@storeCKEditorImages')->name('journals.storeCKEditorImages');
-    Route::resource('journals', 'JournalController');
+        // Comment
+        Route::delete('comments/destroy', [AdminCommentController::class, 'massDestroy'])->name('comments.massDestroy');
+        Route::post('comments/media', [AdminCommentController::class, 'storeMedia'])->name('comments.storeMedia');
+        Route::post('comments/ckmedia', [AdminCommentController::class, 'storeCKEditorImages'])->name('comments.storeCKEditorImages');
+        Route::resource('comments', AdminCommentController::class);
 
-    // Mood Tracker
-    Route::delete('mood-trackers/destroy', 'MoodTrackerController@massDestroy')->name('mood-trackers.massDestroy');
-    Route::resource('mood-trackers', 'MoodTrackerController');
+        // Journal
+        Route::delete('journals/destroy', [AdminJournalController::class, 'massDestroy'])->name('journals.massDestroy');
+        Route::post('journals/media', [AdminJournalController::class, 'storeMedia'])->name('journals.storeMedia');
+        Route::post('journals/ckmedia', [AdminJournalController::class, 'storeCKEditorImages'])->name('journals.storeCKEditorImages');
+        Route::resource('journals', AdminJournalController::class);
 
-    // Mood Tracker Reason
-    Route::delete('mood-tracker-reasons/destroy', 'MoodTrackerReasonController@massDestroy')->name('mood-tracker-reasons.massDestroy');
-    Route::resource('mood-tracker-reasons', 'MoodTrackerReasonController');
+        // Mood Tracker
+        Route::delete('mood-trackers/destroy', [AdminMoodTrackerController::class, 'massDestroy'])->name('mood-trackers.massDestroy');
+        Route::resource('mood-trackers', AdminMoodTrackerController::class);
 
-    // Journey
-    Route::delete('journeys/destroy', 'JourneyController@massDestroy')->name('journeys.massDestroy');
-    Route::resource('journeys', 'JourneyController');
-});
-Route::group(['prefix' => 'profile', 'as' => 'profile.', 'namespace' => 'Auth', 'middleware' => ['auth']], function () {
-    // Change password
-    if (file_exists(app_path('Http/Controllers/Auth/ChangePasswordController.php'))) {
-        Route::get('password', 'ChangePasswordController@edit')->name('password.edit');
-        Route::post('password', 'ChangePasswordController@update')->name('password.update');
-        Route::post('profile', 'ChangePasswordController@updateProfile')->name('password.updateProfile');
-        Route::post('profile/destroy', 'ChangePasswordController@destroy')->name('password.destroyProfile');
-    }
-});
+        // Mood Tracker Reason
+        Route::delete('mood-tracker-reasons/destroy', [AdminMoodTrackerReasonController::class, 'massDestroy'])->name('mood-tracker-reasons.massDestroy');
+        Route::resource('mood-tracker-reasons', AdminMoodTrackerReasonController::class);
+
+        // Journey
+        Route::delete('journeys/destroy', [AdminJourneyController::class, 'massDestroy'])->name('journeys.massDestroy');
+        Route::resource('journeys', AdminJourneyController::class);
+    });
+
+// Profile
+Route::prefix('profile-admin')
+    ->as('profile.')
+    ->middleware('auth')
+    ->group(function () {
+        if (file_exists(app_path('Http/Controllers/Auth/ChangePasswordController.php'))) {
+            Route::get('password', [AuthChangePasswordController::class, 'edit'])->name('password.edit');
+            Route::post('password', [AuthChangePasswordController::class, 'update'])->name('password.update');
+            Route::post('profile', [AuthChangePasswordController::class, 'updateProfile'])->name('password.updateProfile');
+            Route::post('profile/destroy', [AuthChangePasswordController::class, 'destroyProfile'])->name('password.destroyProfile');
+        }
+    });
